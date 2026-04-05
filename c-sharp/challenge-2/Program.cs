3b1b6a692e190b5bc7ab81d9be64e7ed15319bd0
Osama Albahrani <osalbahr@gmail.com>
2026-04-05 19:25:43 +0300

    Add challenge

---
+++ c-sharp/challenge-2/Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "Bob";
+int whole = 3;
+double fraction = 34.4;
+
+Console.WriteLine(
+    $"Hello, {name}! You have {whole} messages in your inbox. The temperature is {fraction} celsius."
+);
