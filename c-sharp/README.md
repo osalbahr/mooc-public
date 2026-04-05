fb816ee58f9a82d9825e08791ec190030ed9eaa1
Osama Albahrani <osalbahr@gmail.com>
2026-04-05 17:42:19 +0300

    Add challenge

---
+++ c-sharp/README.md
@@ -0,0 +1,3 @@
+# C#
+
+- [Free Foundational C# with Microsoft Certification](https://www.freecodecamp.org/learn/foundational-c-sharp-with-microsoft)
---
+++ c-sharp/challenge/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("This is");
+Console.Write(" the ");
+Console.WriteLine("first line.");
+Console.WriteLine("This is the second line.");
